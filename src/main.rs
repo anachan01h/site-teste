ead4f93006485592714ea202b3fc4c372a6d6a74
Ana Carolina <anachan.01h@gmail.com>
2024-08-04 05:24:14 -0300

    Testando Github Pages

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ use std::fs;
 fn main() {
     let page = fs::read_to_string("./template.html").expect("Deveria ter lido o arquivo...");
     let page = page.replace("{}", "Hello World!");
-    fs::write("./output/index.html", page).expect("Não consegui escrever no arquivo ;-;");
+    fs::create_dir_all("./_site").expect("Não consegui criar a pasta ;-;");
+    fs::write("./_site/index.html", page).expect("Não consegui escrever no arquivo ;-;");
 }
